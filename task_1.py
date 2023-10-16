a29386e8c9eb1344ff9553d04446752ab8ea4391
Yurij Orlik <81624225+Louis301@users.noreply.github.com>
2023-10-16 21:36:35 +0300

    Лабораторная работа 3

---
+++ task_1.py
@@ -0,0 +1,17 @@
+def get_item_index(items_list_, item):
+
+    for pos, value in enumerate(items_list_):
+        if value == item:
+            return pos
+
+    return None
+
+
+items_list = ['яблоко', 'банан', 'апельсин', 'груша', 'киви', 'банан']
+
+for find_item in ['банан', 'груша', 'персик']:
+    index_item = get_item_index(items_list, find_item)
+    if index_item is not None:
+        print(f"Первое вхождение товара '{find_item}' имеет индекс {index_item}.")
+    else:
+        print(f"Товар '{find_item}' не найден в списке.")
